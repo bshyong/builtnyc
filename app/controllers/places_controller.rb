79d0c3d9c2b9053f948f64715c39bee3425f017d
ghholt <ghholt>
2013-07-16 18:22:45 -0400

    adds places to the map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,4 +5,36 @@ class PlacesController < ApplicationController
 		redirect_to root_url if @place.nil?
 	end
 
+	def index 
+		places_list = []
+
+		Place.all.each do | p | 
+			places_list<<
+
+			{
+			  type: 'Feature',
+			  geometry: {
+				  type: 'Point',
+				  coordinates: [p.longitude, p.latitude]
+			  },
+			  properties: {
+				  title: p.name,
+				  description: p.name,
+				  'marker-size' => 'small',
+				  'marker-color' => '#f0a'
+			  }
+			}
+		end 
+
+
+		places = { 
+			type: "FeatureCollection", 
+			id:'ghholt.map-7po6ov6d',
+			"features" => places_list 
+		}
+
+		respond_to do | format |
+			format.json{render :json => places}
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Builtnyc::Application.routes.draw do
 
+  get '/place/index', :to => 'places#index'
+
   get '/place(/:id)', :to => 'places#show'
+
 
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
